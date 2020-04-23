c6f59f1ec7d98e42bdc384460a0e69126738d338
Brian Durand <briandurand1990@gmail.com>
2020-04-23 18:04:20 -0300

    Fix users_connected

--- server.js
+++ server.js
@@ -6,16 +6,22 @@ const port = process.env.PORT || 3000;
 
 let users_connected = 0;
 
+//Socket IO
 io.on("connection", (socket) => {
   users_connected += 1;
+
+  socket.on("disconnect", () => {
+    users_connected -= 1;
+  });
 
   socket.on("newMessage", (data) => {
     io.sockets.emit("newMessage", data);
   });
 
-  socket.emit("usersOnLine", users_connected);
+  socket.emit("usersConnected", users_connected);
 });
 
+//Express
 app.use("/", (req, res) => {
   res.send("Un simple server :( no me odies.");
 });
